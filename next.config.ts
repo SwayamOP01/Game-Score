a9f095bb471a62f425f821c8e04cee07136cd184
SwayamOP01 <swayamnagpal94@gmail.com>
2025-10-30 03:12:20 +0530

    Externalize large dependencies in next.config.ts for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        "ffmpeg-static",
+        "ffprobe-static",
+        "@xenova/transformers",
+        "@tensorflow/tfjs-node",
+        "@tensorflow-models/coco-ssd"
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
